a020e4e3521240fe74c2639e7a53d567314f7a33
Ever Orellana <eorellana@applaudostudios.com>
2021-08-24 01:24:33 -0600

    Init Configuration for API and Application solution in order to handle mediatR, automapper, and fluentvalidation library

---
+++ src/Movies.API/Startup.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Movies.Application;
+
+namespace Movies.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("MoviesCatalog",
+                    builder => builder.WithOrigins("https://localhost:44316"));
+            });
+
+            services.InstallMoviesApplication();
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ src/Movies.Application/MoviesApplicationInstaller.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Movies.Application.Common.Behaviours;
+using System.Reflection;
+
+namespace Movies.Application
+{
+    public static class MoviesApplicationInstaller
+    {
+        public static IServiceCollection InstallMoviesApplication(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestValidationBehavior<,>));
+
+            return services;
+        }
+    }
+}
