a6163b02122cb7876237f3d4219e14be00359c52
Ever Orellana <eorellana@applaudostudios.com>
2021-08-27 00:10:54 -0600

    added changes to rate by user

--- src/Movies.Application/MovieRates/Queries/RatesByUser/RateDto.cs
+++ src/Movies.Application/MovieRates/Queries/RatesByUser/RateDto.cs
@@ -1,7 +1,6 @@
 ﻿using AutoMapper;
 using Movies.Application.Common.Mappings;
 using Movies.Domain.Entities;
-using System;
 
 namespace Movies.Application.MovieRates.Queries.RatesByUser
 {
@@ -10,14 +9,12 @@ namespace Movies.Application.MovieRates.Queries.RatesByUser
         public int MovieId { get; set; }
         public string Title { get; set; }
         public string Synopsis { get; set; }
-        public DateTime ReleaseDate { get; set; }
         public void Mapping(Profile profile)
         {
             profile.CreateMap<MovieRate, RateDto>()
                 .ForMember(d => d.MovieId, opt => opt.MapFrom(src => src.MovieId))                
                 .ForMember(d => d.Title, opt => opt.MapFrom(src => src.Movie.Title))
-                .ForMember(d => d.Synopsis, opt => opt.MapFrom(src => src.Movie.Synopsis))
-                .ForMember(d => d.ReleaseDate, opt => opt.MapFrom(src => src.Movie.ReleaseDate));            
+                .ForMember(d => d.Synopsis, opt => opt.MapFrom(src => src.Movie.Synopsis));            
         }
     }
 }
