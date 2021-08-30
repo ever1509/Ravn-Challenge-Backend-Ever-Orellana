64c1c2bd311529c525e5e36c184694e79d530780
Ever Orellana <eorellana@applaudostudios.com>
2021-08-30 10:16:36 -0600

    removed unneccesary property in upload image

--- src/Movies.API/Installers/MediaFileConfiguration.cs
+++ src/Movies.API/Installers/MediaFileConfiguration.cs
@@ -5,7 +5,6 @@ namespace Movies.API.Installers
     public class MediaFileFormRequest
     {
         public IFormFile File { get; set; }
-        public string Folder { get; set; }
         public int Id { get; set; }
     }
 }
